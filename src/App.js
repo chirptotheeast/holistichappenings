5ced8787d4ccf9f808cabb545675c07c2915064e
chirptotheeast <git@github.com>
2021-05-29 17:47:09 -0400

    updating image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import eyebg from './images/eyebg.jpg'
 
 function App() {
   return (
     <div>
       <section id="hero">
-        <header className="App-header border">Holistic Happenings</header>
-        <img src={}/>
+        <div className="container">
+          <header className="App-header border">Holistic Happenings</header>
+          {/* <img src={eyebg} width={900} height={300} /> */}
+        </div>
       </section>
     </div>
   );
